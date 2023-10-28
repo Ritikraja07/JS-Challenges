a7bd3db508ca831120169a26964f6b0b761b24da
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2023-10-28 17:00:15 +0000

    js

---
+++ Assignment_2/name.js
@@ -0,0 +1,24 @@
+let challenges = "30 days of javaScript";
+console.log(challenges);
+console.log(challenges.length);
+console.log(challenges.toUpperCase());
+console.log(challenges.toUpperCase());
+console.log(challenges.substring(4,6));
+console.log(challenges.split());
+console.log(challenges.includes('Script'));
+console.log(challenges.split());
+console.log(challenges.split(" "));
+console.log(challenges.replace('javaScript', 'Python'));
+
+let firstWord = challenges[0];
+console.log(firstWord);
+
+console.log(challenges.indexOf('a'));
+console.log(challenges.lastIndexOf('a'));  //Q. 16
+
+
+
+let company ='Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(company.split());
+console.log(company.charAt(15));
+
