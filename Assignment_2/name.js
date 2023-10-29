fcedb8ef11cbedc5eb2e2126ddc5add782e605cc
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2023-10-29 17:20:21 +0000

    js

--- Assignment_2/name.js
+++ Assignment_2/name.js
@@ -9,6 +9,14 @@ console.log(challenges.includes('Script'));
 console.log(challenges.split());
 console.log(challenges.split(" "));
 console.log(challenges.replace('javaScript', 'Python'));
+console.log(challenges.trim(' '));
+console.log(challenges.startsWith('days'));
+console.log(challenges.endsWith('javaScript'));
+console.log(challenges.match('a'));
+console.log(challenges.concat("Days", "Of", "javaScript"));
+console.log(challenges.repeat(2));
+
+
 
 let firstWord = challenges[0];
 console.log(firstWord);
@@ -21,4 +29,10 @@ console.log(challenges.lastIndexOf('a'));  //Q. 16
 let company ='Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
 console.log(company.split());
 console.log(company.charAt(15));
+
+let sen = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sen.indexOf('because'));
+console.log(sen.lastIndexOf('because'));
+console.log(sen.search('because'));
+
 
